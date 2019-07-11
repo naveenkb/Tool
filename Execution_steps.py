b0198e364525dab273e37dd1595a5db67ff8aa95
naveenkb <veenkb234@gmail.com>
2019-07-11 17:08:32 +0530

    Create Execution_steps.py

---
+++ Execution_steps.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0,'C:/Users/aj34183/Desktop/MST/MST_DevelopedJuly01_v1.0/Desktop_version')
+
+import main as tool
+obj=tool.main()
+obj.modelDev(path='C:/Users/aj34183/Desktop/MST/MST_DevelopedJuly01_v1.0/data' ,version="v1")
